4daf118f20cdbf603e3bc115794784369ac602c8
Gathan Mahesa <gthn217@gmail.com>
2023-07-29 22:28:45 +0700

    Codebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans, Noto_Serif, Poiret_One } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+export const openSans = Open_Sans({ 
+  subsets: ['latin'],
+  weight: ['300','400','500','600','700'],
+  variable: '--font-open-sans'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${openSans.variable} font-sans`}>{children}</body>
     </html>
   )
 }
