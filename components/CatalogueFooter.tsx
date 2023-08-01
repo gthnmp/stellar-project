8f9ed0bf47c956ae71772b1a1ee271f8ae96553a
Gathan Mahesa <gthn217@gmail.com>
2023-08-01 16:02:29 +0700

    Update on footer

--- components/CatalogueFooter.tsx
+++ components/CatalogueFooter.tsx
@@ -1,5 +1,10 @@
 const CatalogueFooter = () => (
-  <footer className="w-screen h-96 bg-purple-500">Foobar</footer>
+  <footer className="w-screen h-96 px-4 py-24">
+    <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-black to-transparent"/>
+    <div className="w-full h-full grid place-items-center">
+      <h1>This is footer</h1>
+    </div>
+  </footer>
 )
 
 export default CatalogueFooter
